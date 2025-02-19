6ec22612a796fb7307fc99632f909e5d46c78bdc
hhhwlgns <hhhwlgns@gmail.com>
2025-02-19 11:19:36 +0900

    day03 postorder

---
+++ day03.py
@@ -0,0 +1,63 @@
+def pre_order(node):
+    if node is None:
+        return
+    print(node.data, end='=>')
+    pre_order(node.left)
+    pre_order(node.right)
+
+def in_order(node):
+    if node is None:
+        return
+    pre_order(node.left)
+    print(node.data, end='=>')
+    pre_order(node.right)
+
+def post_order(node):
+    if node is None:
+        return
+    pre_order(node.left)
+    pre_order(node.right)
+    print(node.data, end='=>')
+
+class TreeNode:
+    def __init__(self):
+        self.data = None
+        self.left = None
+        self.right = None
+
+node1 = TreeNode()
+node1.data = 'hs'
+
+node2 = TreeNode()
+node2.data = 'sl'
+node1.left = node2
+
+node3 = TreeNode()
+node3.data = 'mb'
+node1.right = node3
+
+node4 = TreeNode()
+node4.data = 'hw'
+node2.left = node4
+
+node5 = TreeNode()
+node5.data = 'zz'
+node2.right = node5
+
+node6 = TreeNode()
+node6.data = 'sm'
+node3.left = node6
+
+node7 = TreeNode()
+node7.data = 'dh'
+node4.left = node7
+
+node8 = TreeNode()
+node8.data = 'sn'
+node6.right = node8
+
+pre_order(node1)
+print()
+in_order(node1)
+print()
+post_order(node1)

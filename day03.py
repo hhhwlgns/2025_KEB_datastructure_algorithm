fa64ed7c51a716423da925691574cf0b002828a7
hhhwlgns <hhhwlgns@gmail.com>
2025-02-20 14:30:20 +0900

    day03 tree delete...

--- day03.py
+++ day03.py
@@ -1,20 +1,22 @@
 class TreeNode:
-	def __init__(self):
-		self.left = None
-		self.data = None
-		self.right = None
+    def __init__(self):
+        self.left = None
+        self.right = None
+        self.data = None
+
+    def insert (root, value):
 
 
 if __name__ == "__main__":
-    numbers = [10, 15, 8, 3, 9]
+    groups = ['블랙핑크','레드벨벳','마마무','에이핑크','걸스데이','트와이스']
     root = None
 
     node = TreeNode()
-    node.data = numbers[0]
+    node.data = groups[0]
 
     root = node
 
-    for group in numbers[1:]:
+    for group in groups[1:]:
         node = TreeNode()
         node.data = group
         current = root
@@ -24,7 +26,7 @@ if __name__ == "__main__":
                     current.left = node
                     break
                 current = current.left  # move
-            else:
+            elif group > current.data:
                 if current.right is None:
                     current.right = node
                     break
@@ -32,7 +34,7 @@ if __name__ == "__main__":
 
     print("BST 구성 완료")
 
-    find_group = int(input())
+    find_group = input("찾을 그룹명:")
 
     current = root
     while True:
@@ -51,38 +53,68 @@ if __name__ == "__main__":
             current = current.right
 
 
-delete_group = input()
+    delete_group = input('삭제할 그룹명:')
 
-current = root
-parant = root
-while True:
-    if delete_group == current.data:
+    current = root
+    parent = None
+    while True:
+        if delete_group == current.data:
 
-        if current.left == None and current.right == None:
-            if parant.left == current:
-                parant.left = None
-            elif parant.right == current:
-                parant.right = None
-            del current
-            break
-        elif current.left == None and current.right != None:
-            if parant.left == current:
-                parant.left = current.right
-            elif parant.right == current:
-                parant.right = current.right
-            del current
-            break
-        elif current.left != None and current.right == None:
-            if parant.left == current:
-                parant.left = current.left
-            elif parant.right == current:
-                parant.right = current.left
-            del current
-            break
-        # elif current.left != None and current.right != None:
-        #     if parant.left == current:
-        #         parant.left = current.left
-        #     elif parant.right == current:
-        #         parant.right = current.left
+            if current.left == None and current.right == None:
+                if parent.left == current:
+                    parent.left = None
+                elif parent.right == current:
+                    parent.right = None
+                print(current.data,"을(를) 삭제했습니다.")
+                del current
+                break
+            elif current.left == None and current.right != None:
+                if parent.left == current:
+                    parent.left = current.right
+                elif parent.right == current:
+                    parent.right = current.right
+                print(current.data, "을(를) 삭제했습니다.")
+                del current
+                break
+            elif current.left != None and current.right == None:
+                if parent.left == current:
+                    parent.left = current.left
+                elif parent.right == current:
+                    parent.right = current.left
+                print(current.data, "을(를) 삭제했습니다.")
+                del current
+                break
+            elif current.left != None and current.right != None:
+                current_copy_parent = current
+                current_copy = current.right
+                while current_copy.left is not None:
+                    current_copy_parent = current_copy
+                    current_copy = current_copy.left
+                print(current.data,'을(를) 삭제했습니다.')
+                current.data = current_copy.data
 
-    elif delete_group < current.data:
+                break
+
+        elif delete_group < current.data:
+            if current.left is None:
+                print(delete_group,'이(가) 트리에 없음')
+                break
+            parent = current
+            current = current.left
+
+        elif delete_group > current.data:
+            if current.right is None:
+                print(delete_group, '이(가) 트리에 없음')
+                break
+            parent = current
+            current = current.right
+
+    def inorder_traversal(node):
+        if node is not None:
+            print(node.data)
+            inorder_traversal(node.left)
+            inorder_traversal(node.right)
+
+
+    # BST의 전체 노드 출력
+    inorder_traversal(root)
